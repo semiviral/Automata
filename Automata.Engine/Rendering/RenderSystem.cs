771d69c6d3dd1ef3a3a428ebe664acc1d5b02373
Zavier Divelbiss <semiviral@gmail.com>
2020-11-10 21:33:23 -0600

    RenderSystem documentation

--- Automata.Engine/Rendering/RenderSystem.cs
+++ Automata.Engine/Rendering/RenderSystem.cs
@@ -1,6 +1,7 @@
 #region
 
 using System;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using Automata.Engine.Components;
@@ -36,18 +37,25 @@ namespace Automata.Engine.Rendering
             AutomataWindow.Instance.Resized += GameWindowResized;
 
             _GL = GLAPI.Instance.GL;
-            _GL.ClearColor(0.2f, 0.2f, 0.2f, 1f);
-            _GL.Enable(GLEnum.DepthTest);
-
+            _GL.ClearColor(Color.DimGray);
+            _GL.Enable(EnableCap.DepthTest);
+            
             if (_ENABLE_BACK_FACE_CULLING)
             {
                 // enable and configure face culling
                 _GL.FrontFace(FrontFaceDirection.Ccw);
                 _GL.CullFace(CullFaceMode.Back);
-                _GL.Enable(GLEnum.CullFace);
+                _GL.Enable(EnableCap.CullFace);
             }
 
             _Viewport = new UniformBuffer(_GL, 1, (uint)sizeof(Vector4));
+        }
+
+        private void CheckUpdateViewportUBOAndBind()
+        {
+            if (_NewAspectRatio > 0f) _Viewport.Write(0, new Vector4(0f, 0f, AutomataWindow.Instance.Size.X, AutomataWindow.Instance.Size.Y));
+
+            _Viewport.Bind();
         }
 
         [HandlesComponents(DistinctionStrategy.Any, typeof(Camera), typeof(RenderMesh))]
@@ -56,9 +64,7 @@ namespace Automata.Engine.Rendering
             _GL.Clear((uint)(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
             Span<Plane> planes = stackalloc Plane[Frustum.TOTAL_PLANES];
 
-            if (_NewAspectRatio > 0f) _Viewport.Write(0, new Vector4(0f, 0f, AutomataWindow.Instance.Size.X, AutomataWindow.Instance.Size.Y));
-
-            _Viewport.Bind();
+            CheckUpdateViewportUBOAndBind();
 
             foreach ((IEntity cameraEntity, Camera camera) in entityManager.GetEntities<Camera>())
             {
@@ -69,6 +75,7 @@ namespace Automata.Engine.Rendering
                     ["parameters"] = 128
                 };
 
+                // check for changes and update current camera's view matrix & UBO data
                 if ((cameraEntity.TryGetComponent(out Scale? cameraScale) && cameraScale.Changed)
                     | (cameraEntity.TryGetComponent(out Translation? cameraTranslation) && cameraTranslation.Changed)
                     | (cameraEntity.TryGetComponent(out Rotation? cameraRotation) && cameraRotation.Changed))
@@ -83,6 +90,7 @@ namespace Automata.Engine.Rendering
                     camera.Uniforms.Write(0, camera.View);
                 }
 
+                // if the aspect ratio has changed, update the current camera's projection matrix & UBO data
                 if (_NewAspectRatio > 0f)
                 {
                     camera.Projection = camera.Projector switch
@@ -96,12 +104,15 @@ namespace Automata.Engine.Rendering
                     camera.Uniforms.Write(128, camera.Projection!.Parameters);
                 }
 
+                // if the camera doesn't have a projection, it doesn't make sense to try and render to it
                 if (camera.Projection is null) continue;
 
+                // bind camera's view data UBO and precalculate viewproj matrix
                 camera.Uniforms.Bind();
                 Matrix4x4 viewProjection = camera.View * camera.Projection.Matrix;
                 Material? cachedMaterial = null;
 
+                // iterate each RenderMesh and check if the model matrix needs to be recalculated
                 foreach ((IEntity objectEntity, RenderMesh renderMesh) in entityManager.GetEntities<RenderMesh>())
                     if (((objectEntity.TryGetComponent(out Scale? modelScale) && modelScale.Changed)
                          | (objectEntity.TryGetComponent(out Rotation? modelRotation) && modelRotation.Changed)
@@ -115,6 +126,8 @@ namespace Automata.Engine.Rendering
                         if (modelScale is not null) renderMesh.Model *= Matrix4x4.CreateScale(modelScale.Value);
                     }
 
+                // iterate every valid entity and try to render it
+                // we also sort the entities by their render pipeline ID, so we can avoid doing a ton of rebinding
                 foreach ((IEntity objectEntity, RenderMesh renderMesh, Material material) in entityManager.GetEntities<RenderMesh, Material>()
                     .Where(result => result.Component1.ShouldRender && ((camera.RenderedLayers & result.Component1.Mesh!.Layer) > 0))
                     .OrderBy(result => result.Component2.Pipeline.Handle))
@@ -123,8 +136,10 @@ namespace Automata.Engine.Rendering
 
                     if (objectEntity.TryGetComponent(out OcclusionBounds? bounds) && CheckClipFrustumOcclude(bounds, planes, modelViewProjection)) continue;
 
+                    // conditionally update the currentMaterial if it doesn't match this entity's
                     if (cachedMaterial is null || !material.Equals(cachedMaterial)) ApplyMaterial(material, ref cachedMaterial);
 
+                    // we're about to render, so ensure all of the relevant uniforms are set
                     Matrix4x4.Invert(renderMesh.Model, out Matrix4x4 modelInverted);
                     ShaderProgram vertexShader = cachedMaterial!.Pipeline.Stage(ShaderType.VertexShader);
                     vertexShader.TrySetUniform("_mvp", modelViewProjection);
@@ -137,6 +152,7 @@ namespace Automata.Engine.Rendering
                 }
             }
 
+            // by this point we should've updated all of the projection matrixes, so reset the value to 0.
             _NewAspectRatio = 0f;
         }
 
@@ -160,13 +176,11 @@ namespace Automata.Engine.Rendering
 
         private static void ApplyMaterial(Material material, ref Material? old)
         {
-            // store boolean value for use later
-            bool updatePipeline = !material.Pipeline.Equals(old?.Pipeline);
+            bool updatedPipeline = false;
             ShaderProgram? newFragmentShader = null;
 
-            if (updatePipeline)
+            if (!material.Pipeline.Equals(old?.Pipeline))
             {
-                // bind new pipeline
                 material.Pipeline.Bind();
 
                 // set fragment shader so we can easily bind textures
@@ -179,32 +193,29 @@ namespace Automata.Engine.Rendering
                         old.Textures[index].Bind(TextureUnit.Texture0 + index);
                         newFragmentShader.TrySetUniform($"_tex{index}", index);
                     }
+
+                updatedPipeline = true;
             }
 
-            // if newFragmentShader is null, then we didn't bind a new pipeline
+            // if newFragmentShader is null, then we didn't bind a new pipeline, so use old one
             newFragmentShader ??= old!.Pipeline.Stage(ShaderType.FragmentShader);
-
-            // cache the old texture count, or material count if old is null
             int oldTextureCount = old?.Textures.Count ?? material.Textures.Count;
-
-            // this bool indicates whether we updated any textures
-            bool updateTextures = false;
+            bool updatedTextures = false;
 
             for (int index = 0; index < material.Textures.Count; index++)
             {
-                // if textures match, continue to next
+                // only update textures that are different
                 if ((index < oldTextureCount) && material.Textures[index].Equals(old?.Textures[index])) continue;
 
-                // textures don't match, so reassign this specific texture channel
                 material.Textures[index].Bind(TextureUnit.Texture0 + index);
                 newFragmentShader.TrySetUniform($"_tex{index}", index);
 
-                // we've updated a texture, so set boolean value
-                updateTextures = true;
+                // we've updated a texture, so set flag
+                updatedTextures = true;
             }
 
             // if we've loaded any new material data, replace old material.
-            if (updatePipeline || updateTextures) old = material;
+            if (updatedPipeline || updatedTextures) old = material;
         }
 
         private void GameWindowResized(object sender, Vector2i newSize) => _NewAspectRatio = (float)newSize.X / (float)newSize.Y;
