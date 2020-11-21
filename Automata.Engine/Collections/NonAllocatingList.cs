be55089bd25553043ca3d354f7922f92993c1b9c
Zavier Divelbiss <semiviral@gmail.com>
2020-11-21 02:43:55 -0600

    extensive documentation for NonAllocatingList

--- Automata.Engine/Collections/NonAllocatingList.cs
+++ Automata.Engine/Collections/NonAllocatingList.cs
@@ -2,6 +2,7 @@ using System;
 using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Automata.Engine.Collections
 {
@@ -13,11 +14,32 @@ namespace Automata.Engine.Collections
 
         private T[] _InternalArray;
 
+        /// <summary>
+        ///     Whether the <see cref="NonAllocatingList{T}" /> has been disposed.
+        /// </summary>
         public bool Disposed { get; private set; }
 
+        /// <summary>
+        ///     Wraps the currently addressed section (determined by <see cref="Count" />) of the internal array.
+        /// </summary>
         public Span<T> Segment => new(_InternalArray, 0, Count);
+
+        /// <summary>
+        ///     Whether the <see cref="NonAllocatingList{T}" /> is empty.
+        /// </summary>
         public bool IsEmpty => Count <= 0;
 
+        /// <summary>
+        ///     The current total capacity of the internal array.
+        /// </summary>
+        public int Capacity => _InternalArray.Length;
+
+        /// <summary>
+        ///     Sets or gets the element at the given index.
+        /// </summary>
+        /// <param name="index">
+        ///     Index to get or set element at.
+        /// </param>
         public T this[uint index]
         {
             get
@@ -69,8 +91,167 @@ namespace Automata.Engine.Collections
             }
         }
 
+        /// <summary>
+        ///     Copies the contents of the <see cref="NonAllocatingList{T}" /> to the destination <see cref="Span{T}" />.
+        /// </summary>
+        /// <param name="destination">
+        ///     <see cref="Span{T}" /> to copy contents to.
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void CopyTo(Span<T> destination) => Segment.CopyTo(destination);
+
+        /// <summary>
+        ///     Fills the <see cref="NonAllocatingList{T}" /> with the given item.
+        /// </summary>
+        /// <param name="item">
+        ///     Item to fill with.
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Fill(T item) => Segment.Fill(item);
+
+        /// <summary>
+        ///     Clears the contents of the <see cref="NonAllocatingList{T}" />, and optionally returns its used memory (down to the default capacity).
+        /// </summary>
+        /// <param name="compress">
+        ///     Whether to compress the capacity of the <see cref="NonAllocatingList{T}" />.
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear(bool compress)
+        {
+            if (Count > 0)
+            {
+                return;
+            }
+
+            Segment.Clear();
+            Count = 0;
+
+            if (compress)
+            {
+                ArrayPool<T>.Shared.Return(_InternalArray);
+                _InternalArray = ArrayPool<T>.Shared.Rent(_DEFAULT_CAPACITY);
+            }
+        }
+
+        /// <summary>
+        ///     The current count of items.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        ///     Whether the <see cref="NonAllocatingList{T}" /> is read only.
+        /// </summary>
+        public bool IsReadOnly => Disposed;
+
+        /// <summary>
+        ///     Sets or gets the element at the given index.
+        /// </summary>
+        /// <param name="index">
+        ///     Index to get or set element at.
+        /// </param>
+        public T this[int index] { get => _InternalArray[(uint)index]; set => _InternalArray[(uint)index] = value; }
+
+        /// <summary>
+        ///     Determine whether or not the <see cref="NonAllocatingList{T}" /> contains the given item.
+        /// </summary>
+        /// <param name="item">
+        ///     Item to search for.
+        /// </param>
+        /// <returns>
+        ///     <c>True</c> if the item is found, otherwise <c>False</c>.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Contains(T item) => (Count != 0) && (IndexOf(item) != -1);
+
+        /// <summary>
+        ///     Find the first occuring index of the given item.
+        /// </summary>
+        /// <param name="item">
+        ///     Item to search for.
+        /// </param>
+        /// <returns>
+        ///     Returns -1 if the item is not found, otherwise returns the index the item was found at.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int IndexOf(T item) => Segment.IndexOf(item);
+
+
+        #region Adding / Inserting
+
+        /// <summary>
+        ///     Adds the an item to the end of the <see cref="NonAllocatingList{T}" />.
+        /// </summary>
+        /// <remarks>
+        ///     If the <see cref="NonAllocatingList{T}" />'s capacity cannot hold value, the capacity is doubled.
+        /// </remarks>
+        /// <param name="item">Item to add.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(T item)
+        {
+            if (Count >= _InternalArray.Length)
+            {
+                EnsureCapacityOrResize(Count + 1);
+            }
+
+            _InternalArray[Count] = item;
+            Count += 1;
+        }
+
+        /// <summary>
+        ///     Adds a given range of items.
+        /// </summary>
+        /// <remarks>
+        ///     If the <see cref="NonAllocatingList{T}" />'s capacity cannot hold the items, the capacity is doubled.
+        /// </remarks>
+        /// <param name="items">Items to add.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void AddRange(ReadOnlySpan<T> items) => InsertRange(Count, items);
 
+        /// <summary>
+        ///     Inserts an item at a particular index.
+        /// </summary>
+        /// <remarks>
+        ///     If the <see cref="NonAllocatingList{T}" />'s capacity cannot hold the items, the capacity is doubled.
+        /// </remarks>
+        /// <param name="index">
+        ///     Index to insert item at.
+        /// </param>
+        /// <param name="item">
+        ///     Item to insert.
+        /// </param>
+        public void Insert(int index, T item)
+        {
+            if (index > Count)
+            {
+                ThrowHelper.ThrowIndexOutOfRangeException();
+            }
+            else if (index == Count)
+            {
+                EnsureCapacityOrResize(Count + 1);
+            }
+
+            // this copies everything from index..Count to index + 1
+            else if (index < Count)
+            {
+                Segment.Slice(index).CopyTo(_InternalArray.AsSpan().Slice(index + 1));
+            }
+
+            _InternalArray[index] = item;
+            Count += 1;
+        }
+
+        /// <summary>
+        ///     Inserts a range of items, starting from a given index.
+        /// </summary>
+        /// <remarks>
+        ///     If the <see cref="NonAllocatingList{T}" />'s capacity cannot hold the items, the capacity is doubled.
+        /// </remarks>
+        /// <param name="index">
+        ///     Index to insert items at.
+        /// </param>
+        /// <param name="items">
+        ///     Items to insert.
+        /// </param>
         public void InsertRange(int index, ReadOnlySpan<T> items)
         {
             if ((uint)index > (uint)Count)
@@ -95,6 +276,7 @@ namespace Automata.Engine.Collections
             Count += items.Length;
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         private void EnsureCapacityOrResize(int minimumCapacity)
         {
             if (_InternalArray.Length >= minimumCapacity)
@@ -110,48 +292,20 @@ namespace Automata.Engine.Collections
             _InternalArray = newArray;
         }
 
-        public void CopyTo(Span<T> span) => Segment.CopyTo(span);
+        #endregion
 
-        public void Fill(T item) => Segment.Fill(item);
 
-        public bool IsReadOnly => Disposed;
+        #region Removing
 
-        public int Count { get; private set; }
-
-        public T this[int index] { get => _InternalArray[(uint)index]; set => _InternalArray[(uint)index] = value; }
-
-        public void Add(T item)
-        {
-            if (Count >= _InternalArray.Length)
-            {
-                EnsureCapacityOrResize(Count + 1);
-            }
-
-            _InternalArray[Count] = item;
-            Count += 1;
-        }
-
-        public void Insert(int index, T item)
-        {
-            if (index > Count)
-            {
-                ThrowHelper.ThrowIndexOutOfRangeException();
-            }
-            else if (index == Count)
-            {
-                EnsureCapacityOrResize(Count + 1);
-            }
-
-            // this copies everything from index..Count to index + 1
-            else if (index < Count)
-            {
-                Segment.Slice(index).CopyTo(_InternalArray.AsSpan().Slice(index + 1));
-            }
-
-            _InternalArray[index] = item;
-            Count += 1;
-        }
-
+        /// <summary>
+        ///     Removes an item from the <see cref="NonAllocatingList{T}" />.
+        /// </summary>
+        /// <param name="item">
+        ///     Item to remove.
+        /// </param>
+        /// <returns>
+        ///     <c>True</c> if the item was removed, otherwise <c>False</c>.
+        /// </returns>
         public bool Remove(T item)
         {
             int index = IndexOf(item);
@@ -165,6 +319,12 @@ namespace Automata.Engine.Collections
             return true;
         }
 
+        /// <summary>
+        ///     Removes the item at the given index.
+        /// </summary>
+        /// <param name="index">
+        ///     Index to remove item at.
+        /// </param>
         public void RemoveAt(int index)
         {
             if (index > Count)
@@ -185,15 +345,18 @@ namespace Automata.Engine.Collections
             Count -= 1;
         }
 
-        public bool Contains(T item) => (Count != 0) && (IndexOf(item) != -1);
+        #endregion
 
-        public int IndexOf(T item) => Segment.IndexOf(item);
 
-        public void CopyTo(T[] array, int arrayIndex) => Segment.CopyTo(new Span<T>(array, arrayIndex, array.Length - arrayIndex));
+        #region ICollection
 
-        public void Clear()
+        /// <inheritdoc cref="CopyTo" />
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex) => Segment.CopyTo(new Span<T>(array, arrayIndex, array.Length - arrayIndex));
+
+        /// <inheritdoc cref="Clear" />
+        void ICollection<T>.Clear()
         {
-            if (Count == 0)
+            if (Count > 0)
             {
                 return;
             }
@@ -201,6 +364,8 @@ namespace Automata.Engine.Collections
             Segment.Clear();
             Count = 0;
         }
+
+        #endregion
 
 
         #region IEnumerable
